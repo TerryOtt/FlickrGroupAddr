790b6c555e3f8c6acade84e05a31ed10d3002edc
Terry Ott <Terry.Ott@gmail.com>
2020-10-25 18:01:28 +0000

    Ping operation working, let's do auth

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FlickrGroupAddr
+
+## Prerequisites
+
+### Python modules
+
+```
+$ sudo apt-get -y install python3-pip certbot
+$ sudo pip3 install tornado
+```
+
+### LetsEncrypt
+
+```
+$ sudo apt-get -y install certbot python3-certbot
+$ sudo certbot certonly --standalone -d groupaddrapi.sixbuckssolutions.com
+```
---
+++ src/group-addr-endpoint.py
@@ -0,0 +1,116 @@
+import tornado.ioloop
+import tornado.web
+import os
+import tornado.tcpserver
+import ssl
+import logging
+
+
+class FlickrGroupAddrEndpointHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin",      "*")
+        self.set_header("Access-Control-Allow-Headers",     "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods',     "GET, OPTIONS")
+
+    
+    def options(self):
+        # no body
+        self.set_status(204)
+        self.finish()
+
+
+    def get(self, operation ):
+        valid_operations = {
+            "ping"          : self._do_ping,
+        }
+
+        if operation in valid_operations:
+            logging.info( "Valid operation requested: {0}".format(operation) )
+            valid_operations[ operation ]()
+        else:
+            error_string = "Invalid operation requested: \"{0}\"".format(operation)
+            self._set_status( 404, error_string )
+            self.write( 
+                { 
+                    "status"        : "error",
+                    "error_info"    : error_string
+                }
+            )
+
+
+    def _do_ping(self):
+        self.write( 
+            { 
+                "result"    : "pong " 
+            }
+        )
+
+
+def _make_app():
+    return tornado.web.Application(
+        [
+            (r"^\/api/v1/(.+)\s*$", FlickrGroupAddrEndpointHandler ),
+        ],
+        debug=True
+    )
+
+
+def _quiet_other_loggers():
+    other_loggers = [
+        "asyncio",
+    ]
+ 
+    for curr_logger in other_loggers:
+        logging.getLogger( curr_logger ).setLevel( logging.WARN )
+
+
+def _make_ssl_ctx():
+    ssl_ctx = ssl.create_default_context( ssl.Purpose.CLIENT_AUTH )
+    crt_file = '/etc/letsencrypt/live/groupaddrapi.sixbuckssolutions.com/fullchain.pem'
+    key_file = '/etc/letsencrypt/live/groupaddrapi.sixbuckssolutions.com/privkey.pem'
+
+    logging.info( "TLS certificate and chain: {0}\n     TLS private key file: {1}".format(crt_file, key_file) )
+
+    ssl_ctx.minimum_version = ssl.TLSVersion.TLSv1_2
+
+    acceptable_cipher_suites = [
+        "ECDHE-ECDSA-AES128-GCM-SHA256",
+        "ECDHE-ECDSA-AES256-GCM-SHA384",
+        #"ECDHE-ECDSA-AES128-SHA",
+        #"ECDHE-ECDSA-AES256-SHA",
+        #"ECDHE-ECDSA-AES128-SHA256",
+        #"ECDHE-ECDSA-AES256-SHA384",
+        "ECDHE-RSA-AES128-GCM-SHA256",
+        "ECDHE-RSA-AES256-GCM-SHA384",
+        #"ECDHE-RSA-AES128-SHA",
+        #"ECDHE-RSA-AES256-SHA",
+        #"ECDHE-RSA-AES128-SHA256",
+        #"ECDHE-RSA-AES256-SHA384",
+        "DHE-RSA-AES128-GCM-SHA256",
+        "DHE-RSA-AES256-GCM-SHA384",
+        #"DHE-RSA-AES128-SHA",
+        #"DHE-RSA-AES256-SHA",
+        #"DHE-RSA-AES128-SHA256",
+        #"DHE-RSA-AES256-SHA256",
+    ]
+
+    ssl_ctx.set_ciphers( ":".join(acceptable_cipher_suites) )
+
+    ssl_ctx.load_cert_chain( crt_file, key_file ) 
+
+    return ssl_ctx
+
+if __name__ == "__main__":
+    logging.basicConfig( level=logging.DEBUG )
+    _quiet_other_loggers()
+    application = _make_app()
+    ssl_ctx = _make_ssl_ctx()
+    
+    http_server = tornado.httpserver.HTTPServer( application, ssl_options=ssl_ctx )
+    server_port = 443
+
+    http_server.listen( server_port )
+
+    logging.info( "Listening for HTTPS connections on port {0}".format(server_port) )
+
+    tornado.ioloop.IOLoop.current().start()
